ba9cd5a02b44c22bfeec32e6e831947aa5fe34c3
tetsugray <tetsugray@gmail.com>
2020-11-18 03:31:02 +0300

    find_form

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
-
+import 'root/js/jquery_plugins/datepicker/datepicker.js';
+import 'root/js/jquery_plugins/dropdown_quality/item-quantity-dropdown.min.js';
+import 'root/src/test/test.js'
 //импорт стилей
 import 'root/src/layouts/scss/common.scss';
- 
+import 'root/js/jquery_plugins/datepicker/datepicker.css';
+import 'root/js/jquery_plugins/dropdown_quality/dropdown_quality.css'
